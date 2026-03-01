7435844757f20ef352c84d9354e21e946affe170
chadhas282-afk <chadhas282@gmail.com>
2026-03-01 17:20:46 +0530

    add routes to the GPT

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import SideBar from './components/SideBar';
+import ChatBox from './components/ChatBox';
+import Credits from './pages/Credits';
+import Community from './pages/community';
 
 const App = () => {
   return (
-    <div>
-      
+    <>
+    <div className='dark:bg-linear-to-b from-[#242124] to-[#000000] dark:text-white'>
+        <div className='flex h-screen w-screen'>
+        <SideBar />
+        <Routes>
+          <Routes path="/" element={<ChatBox />} />
+          <Routes path="/Credits" element={<Credits />} />
+          <Routes path="/Community" element={<Community />} />
+        </Routes>
+      </div>
     </div>
+    </>
   );
 }
 
--- src/pages/Credits.jsx
+++ src/pages/Credits.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Login = () => {
+const Credits = () => {
   return (
     <div>
-      Login
+      Credits
     </div>
   );
 }
 
-export default Login;
+export default Credits;
