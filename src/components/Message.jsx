2df4e8310179b4017b7ed8cd1a141ae9555bb375
chadhas282-afk <chadhas282@gmail.com>
2026-03-02 20:57:42 +0530

    fix: format message timestamps to display relative time

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { assets } from '../assets/assets';
+import moment from 'moment';
 
 const Message = ({Message}) => {
   return (
@@ -10,7 +11,7 @@ const Message = ({Message}) => {
             <div className='flex flex-col gap-2 p-2 px-4 bg-slate-50 dark:bg-[#57317C]/30 border border-[#80609F]/30 rounded-md max-w-2xl'>
               <p className='text-sm dark:text-primary'>{Message.content}</p>
               <span className='text-xs text-gray-400 dark:text-[#B1A6X0]'>
-                {Message.timestamp}
+                {moment(Message.timestamp).fromNow()}
               </span>
             </div>
             <img src={assets.user_icon} alt="" className='w-8 rounded-full'/>
@@ -25,7 +26,7 @@ const Message = ({Message}) => {
               )
             }
             <span>
-              {Message.timestamp}
+              {moment(Message.timestamp).fromNow()}
             </span>
           </div>
         )
