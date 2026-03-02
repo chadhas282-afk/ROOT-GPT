c4dd8c8af067a889cfbf07c7479279be4d3bfeb3
chadhas282-afk <chadhas282@gmail.com>
2026-03-02 21:14:03 +0530

    feat: add loading spinner to ChatBox component

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -31,6 +31,13 @@ const ChatBox = () => {
             <Message key={index} Message={message} />
           ))
         }
+        {
+          loading && <div className='loader flex items-center gap-1.5'>
+            <div className='w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce'></div>
+            <div className='w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce'></div>
+            <div className='w-1.5 h-1.5 rounded-full bg-gray-500 dark:bg-white animate-bounce'></div>
+          </div>
+        }
     </div>
 
     <form>
