cef83b039cc55a0877fcbf60cd747a0a7b9aeaff
chadhas282-afk <chadhas282@gmail.com>
2026-03-03 17:39:32 +0530

    made user login schema

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,11 +1,51 @@
+import User from "../models/user";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
+}
+
 export const registerUser = async (req, res) => {
     const { name, email, password } = req.body;
 
     try {
-        const newUser = new User({ name, email, password });
-        await newUser.save();
-        res.status(201).json({ message: "User registered successfully", user: newUser });
-    } catch (error) {
-        res.status(500).json({ message: "Error registering user", error: error.message });
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            return res.json({success:false,  message: "User already exists" });
+        }
+        const user = await User.create({ name, email, password });
+        const token = generateToken(user._id);
+        res.json({ success: true, token });
+    } catch(error){
+        res.json({ success: false, error: error.message });
     }
-};
+}
+
+export const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+        if (user) {
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) {
+                const token = generateToken(user._id);
+                return res.json({ success: true, token });
+            }
+        }
+        return res.json({ success: false, message: "Invalid email or password" });
+    } catch (error) {
+        res.json({ success: false, error: error.message });
+    }
+}
+
+export const getUser = async (req, res) => {
+    try {
+        const user = req.user;
+        return res.json({ success: true, user });
+    } catch (error) {
+        res.json({ success: false, error: error.message });
+    }
+}
