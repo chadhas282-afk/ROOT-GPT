cf3fa60a1d504a63d3bac21b58c0ddd0cfb7ec7c
chadhas282-afk <chadhas282@gmail.com>
2026-03-03 16:53:56 +0530

    made user schema

---
+++ server/controllers/userController.js
@@ -0,0 +1,11 @@
+export const registerUser = async (req, res) => {
+    const { name, email, password } = req.body;
+
+    try {
+        const newUser = new User({ name, email, password });
+        await newUser.save();
+        res.status(201).json({ message: "User registered successfully", user: newUser });
+    } catch (error) {
+        res.status(500).json({ message: "Error registering user", error: error.message });
+    }
+};
---
+++ server/models/user.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+import { use } from "react";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    Credits: {
+        type: Number,
+        default: 20
+    }
+});
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+
+export default User;
