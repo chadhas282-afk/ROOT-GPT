0b1c5fb3371e361b9c7befc4eb2599ff75f87b94
chadhas282-afk <chadhas282@gmail.com>
2026-03-03 11:54:37 +0530

    feat: implement auto-scrolling in ChatBox component

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -3,8 +3,11 @@ import { useState } from 'react';
 import { useAppContext } from '../context/AppContext'
 import { assets } from "../assets/assets";
 import Message from './Message';
+import { useRef } from 'react';
 
 const ChatBox = () => {
+
+  const containerRef = useRef(null);
 
   const { selectedChats, theme } = useAppContext();
   const [messages, setMessages] = useState([]);
@@ -24,10 +27,17 @@ const ChatBox = () => {
   }
 }, [selectedChats]);
 
+  useEffect(() => {
+    if (containerRef.current) {
+      containerRef.current.scrollTop = containerRef.current.scrollHeight;
+      containerRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages]);
+
   return (
     <div className='flex-1 flex flex-col justify-between m-5 md:m-10 xl:mx-30 max-md:mt-14 2xl:pr-40'>
-      
-    <div className='flex-1 mb-5 overflow-y-scroll'>
+
+    <div ref={containerRef} className='flex-1 mb-5 overflow-y-scroll'>
       {
         messages.length === 0 && (
           <div className='h-full flex flex-col items-center justify-center gap-2 text-primary'>
