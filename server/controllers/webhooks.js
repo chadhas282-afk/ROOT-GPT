4182bf44f172ef9fb69c8147437d79e27d47f062
chadhas282-afk <chadhas282@gmail.com>
2026-03-05 18:50:54 +0530

    fix: correct import case for User model in webhooks controller

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 import Transaction from "../models/Transaction.js";
-import User from "../models/User.js";
+import User from "../models/user.js";
 
 
 export const stripeWebhooks = async (req, res) => {
